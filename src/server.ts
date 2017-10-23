64e1bec85c7bb56d2b0492a8ebe9717fc5c81e4d
Ezeki Zibzibadze <playerx@users.noreply.github.com>
2017-10-23 10:00:34 +0400

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import startGraphQLServer from './common/server.hapi';
 import schema from './schema';
 
 startGraphQLServer(
-	schema,			// our schema
-	'localhost',	// for GraphiQL subscriptions
-	3000, 			// development port
+	schema,				// our schema
+	'localhost',			// for GraphiQL subscriptions
+	process.env.PORT || 3000,	// development port
 );
