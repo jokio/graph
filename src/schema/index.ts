0634407da859a2c6e43e42cf4d59d62fe012535c
Ezeki Zibzibadze <ez@jok.io>
2017-10-23 23:43:27 +0400

    updated deployment configuration

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,9 +1,10 @@
 import { makeExecutableSchema } from 'graphql-tools';
 import { mergeTypes, mergeResolvers } from 'merge-graphql-schemas';
 import fileLoader from '../common/loader';
+import * as path from 'path';
 
-const typeDefs = fileLoader(__dirname, { recursive: true, patternString: '\.gql' });
-const resolvers = fileLoader(__dirname, { recursive: true, patternString: 'resolvers\.ts' });
+const typeDefs = fileLoader(path.join(__dirname, '../../src/schema'), { recursive: true, patternString: '\.gql' });
+const resolvers = fileLoader(__dirname, { recursive: true, patternString: 'resolvers\.' });
 
 const executableSchema = makeExecutableSchema({
     typeDefs: mergeTypes(typeDefs),
