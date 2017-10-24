ccb7a03d707e41b7af6aadd7caeff20b7d0e95ea
Ezeki Zibzibadze <ez@jok.io>
2017-10-25 02:09:19 +0400

    updated pubsub

--- src/common/pubsub.ts
+++ src/common/pubsub.ts
@@ -5,11 +5,17 @@ export const pubsub = new PubSub();
 
 const REDIS_CONFIG = process.env.REDIS_CONFIG;
 
+console.log('REDIS_CONFIG', REDIS_CONFIG)
+
 if (REDIS_CONFIG) {
+    const configParams = REDIS_CONFIG.split(':');
+
+    console.log('configParams', configParams)
+
     const redis = new Redis({
-        host: REDIS_CONFIG.split(':')[0],
-        port: +REDIS_CONFIG.split(':')[1],
-        password: REDIS_CONFIG.split(':')[2],
+        host: configParams[0],
+        port: +configParams[1],
+        password: configParams[2],
     });
 
     redis.subscribe('musicChannelTrackUpdated', x => {
